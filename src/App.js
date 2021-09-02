19386330fcdeddc6a919ec96b213009bc239bc2f
Garrett Ross <garrettross@gGarrettRoss.local>
2021-09-02 11:15:30 -0500

    assessment repo

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Make state of loading, users, showAdress */}
+      {/* Make componentdidmount that reaches out to 
+        https://jsonplaceholder.typicode.com/users
+      */}
+    {/* Set the users state with data you get back */}
+    {/* Set loading to true while data is being fetched and render loading component */}
+    {/* when loading is false (conditional rendering) render the users component that loops through props of users and renders userItem component */}
+    {/* Look at data you get back. Notice what you have access to */}
+    {/* create a button in the users component that allows change in the showAddress(boolean) state */}
+      {/* If the showaddress boolean is true, show all addresses of users */}
     </div>
   );
 }
